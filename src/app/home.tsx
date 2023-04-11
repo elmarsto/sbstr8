681a18042017b5fbf82be224af8dc81ac64e50f1
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2023-04-11 13:21:19 -0700

    fix(cleanup): simplified some code

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,7 +1,6 @@
 // adapted from https://raw.githubusercontent.com/Ibaslogic/nextjs-mdx-blog-starter/main/pages/blog/index.js
 
 import Link from 'next/link';
-import { format } from 'date-fns';
 import { PostMetadata } from '@/utils/post';
 import config from '@/../martrix-config';
 import styles from './styles.module.css';
@@ -10,32 +9,25 @@ export interface HomeProps {
   items: PostMetadata[];
 }
 
-export const Home = ({ items }: HomeProps) => {
-  console.log(items);
-  return (
-    <main>
-      <h1>{config.title} - Posts</h1>
-      <ul className={styles['posts']}>
-        {items.map((it: PostMetadata) => {
-          const { slug, title, date, description }: PostMetadata = it;
-
-          return (
-            <li className={styles['post']} key={slug}>
-              <Link className={styles['link']} href={`/post/${slug}`}>
-                <article>
-                  <h2>{title}</h2>
-                  <div className={styles['metadata']}>
-                    <span>{date}</span>{' '}
-                  </div>
-                  <p>{description}</p>
-                </article>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </main>
-  );
-};
+export const Home = ({ items }: HomeProps) => (
+  <main>
+    <h1>{config.title}</h1>
+    <ul className={styles['posts']}>
+      {items.map(({ slug, title, date, description }: PostMetadata) => (
+        <li className={styles['post']} key={slug}>
+          <Link className={styles['link']} href={`/post/${slug}`}>
+            <article>
+              <h2>{title}</h2>
+              <div className={styles['metadata']}>
+                <span>{date}</span>{' '}
+              </div>
+              <p>{description}</p>
+            </article>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </main>
+);
 
 export default Home;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,12 @@
+import React from 'react';
 import cfg from '@/../martrix-config';
 
-export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
+const RootLayout = ({ children }: React.PropsWithChildren<unknown>) => (
+  <html lang="en">
+    <body>{children}</body>
+  </html>
+);
+export default RootLayout;
 
 export const metadata = {
   title: cfg.title,
