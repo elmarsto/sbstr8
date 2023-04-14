38189912497066abe816e6de202fc96ff9ad3133
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2023-04-13 23:55:22 -0700

    feat(graphql): added apollo sever; bumped nextjs canary en passant

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,30 @@
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { ApolloServer } from '@apollo/server';
+import { gql } from 'graphql-tag';
+
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+});
+
+const handler = startServerAndCreateNextHandler(server);
+
+export async function GET(request) {
+  return handler(request);
+}
+
+export async function POST(request) {
+  return handler(request);
+}
