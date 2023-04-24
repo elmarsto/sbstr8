3e5ea687c411e35a53c583146a18770e4d406c63
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2023-04-23 23:47:02 -0700

    fix(rename): rename

--- sbstr8-config.js
+++ sbstr8-config.js
@@ -4,20 +4,19 @@ exports.defaultAuthor = void 0;
 const env = process.env.NODE_ENV;
 const isProd = env === 'production';
 exports.defaultAuthor = {
-    name: 'You',
-    email: 'you@somewhere',
-    link: 'https://you.somewhere/',
-    twitter: '@you',
+  name: 'You',
+  email: 'you@somewhere',
+  link: 'https://you.somewhere/',
+  twitter: '@you',
 };
 const config = {
-    title: 'Substrate',
-    description: 'A maximalist storytelling engine',
-    link: isProd ? 'http://127.0.0.1:3000' : 'http://127.0.0.1:3000',
-    categories: ['Storytelling', 'Journalism'],
-    keywords: ['storytelling', 'journalism', 'data', 'visualization'],
-    language: 'en',
-    copyright: '2023 All rights reserved.',
-    owners: [exports.defaultAuthor],
+  title: 'sbstr8',
+  description: 'A maximalist storytelling engine',
+  link: isProd ? 'http://127.0.0.1:3000' : 'http://127.0.0.1:3000',
+  categories: ['Storytelling', 'Journalism'],
+  keywords: ['storytelling', 'journalism', 'data', 'visualization'],
+  language: 'en',
+  copyright: '2023 All rights reserved.',
+  owners: [exports.defaultAuthor],
 };
 exports.default = config;
-//# sourceMappingURL=substrate-config.js.map
