3d1cdc6b3b598d13d5ecba2fcf89b9c0245dee52
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2023-04-24 00:48:30 -0700

    fix(alpha): alpha sort config

--- sbstr8-config.ts
+++ sbstr8-config.ts
@@ -2,19 +2,19 @@ import { Config } from '@/types/config';
 export const env = process.env.NODE_ENV;
 export const isProd = env === 'production';
 export const defaultAuthor = {
-  name: 'You',
   email: 'you@somewhere',
   link: 'https://you.somewhere/',
+  name: 'You',
   twitter: '@you',
 };
 export const config: Config = {
-  title: 'sbstr8',
-  description: 'A maximalist storytelling engine',
-  link: isProd ? 'http://127.0.0.1:3000' : 'http://127.0.0.1:3000',
   categories: ['Storytelling', 'Journalism'],
+  copyright: '2023 All rights reserved.',
+  description: 'A maximalist storytelling engine',
   keywords: ['storytelling', 'journalism', 'data', 'visualization'],
   language: 'en',
-  copyright: '2023 All rights reserved.',
+  link: isProd ? 'http://127.0.0.1:3000' : 'http://127.0.0.1:3000',
   owners: [exports.defaultAuthor],
+  title: 'sbstr8',
 };
 export default config;
