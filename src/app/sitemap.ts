c9841292f454367cef9a63d82343928b1a6561e8
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2023-06-26 18:34:46 -0700

    chore(refactor): sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,1 @@
-import { defaultPostPath, getSortedPost, getLastModified } from '@/lib/post';
-import cfg from '@/../sbstr8-config';
-import urlJoin from 'url-join';
-
-export default function sitemap() {
-  const posts = getSortedPost();
-  const lastModified = getLastModified(posts).toISOString();
-  const sitemap = (posts || []).map(({ slug, updated, created }) => ({
-    url: urlJoin(cfg.link, cfg.postPath || defaultPostPath, slug),
-    lastModified: updated || created || new Date().toISOString(),
-  }));
-  sitemap.push({ url: cfg.link, lastModified });
-  return sitemap;
-}
+export * from '@/sbstr8/lib/sitemap';
---
+++ src/sbstr8/lib/sitemap.ts
@@ -0,0 +1,25 @@
+import {
+  defaultPostPath,
+  getSortedPost,
+  getLastModified,
+} from '@/sbstr8/lib/post';
+import cfg from '@/sbstr8/config';
+import urlJoin from 'url-join';
+import { menuLinks } from '@/sbstr8/lib/menu';
+
+export default function sitemap() {
+  const posts = getSortedPost();
+  const lastModified = getLastModified(posts).toISOString();
+  const sitemap = (posts || []).map(({ slug, updated, created }) => ({
+    url: urlJoin(cfg.link, cfg.postPath || defaultPostPath, slug),
+    lastModified: updated || created || new Date().toISOString(),
+  }));
+  sitemap.push({ url: cfg.link, lastModified });
+  menuLinks.map((ml) => {
+    if (/^(http|mailto).*$/.test(ml.href)) {
+      return;
+    }
+    sitemap.push({ url: urlJoin(cfg.link, ml.href), lastModified });
+  });
+  return sitemap;
+}
