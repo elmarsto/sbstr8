03b61472ad9a3ddef0a1e79308f8dc526176e308
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2023-07-07 16:09:07 -0700

    feat(css): tailwind colors as css vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: { },
-  plugins: [],
+  theme: {},
+  plugins: [
+    // adapted from https://gist.github.com/Merott/d2a19b32db07565e94f10d13d11a8574
+    ({ addBase, theme }) => {
+      const extractColorVars = (colorObj, colorGroup = '') => {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey];
+
+          const newVars =
+            typeof value === 'string'
+              ? { [`--color${colorGroup}-${colorKey}`]: value }
+              : extractColorVars(value, `-${colorKey}`);
+
+          return { ...vars, ...newVars };
+        }, {});
+      };
+
+      addBase({
+        ':root': extractColorVars(theme('colors')),
+      });
+    },
+  ],
 };
